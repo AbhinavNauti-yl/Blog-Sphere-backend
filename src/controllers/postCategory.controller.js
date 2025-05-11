1eadb83448079733b7808ccf80c3a5e2486e43e8
AbhinavNauti-yl <abhinavnautiyal2004@gmail.com>
2025-05-11 21:03:08 +0530

    add pagination and search functionality to getAllPostCategory; implement getPostCategory route

--- src/controllers/postCategory.controller.js
+++ src/controllers/postCategory.controller.js
@@ -23,17 +23,6 @@ const createPostCategory = asyncHandeler(async (req, res, next) => {
     .json(new apiResponse(201, savedPostCategory, "new post Category created"));
 });
 
-const getAllPostCategory = asyncHandeler(async (req, res, next) => {
-  const postCategories = await PostCategories.find({});
-  if (!postCategories) {
-    throw new apiError(404, "no post Category found");
-  }
-
-  res
-    .status(200)
-    .json(new apiResponse(200, postCategories, "all Post Categories"));
-});
-
 const updatePostCategory = asyncHandeler(async (req, res, next) => {
   const { title } = req.body;
   const updatedPostCategory = await PostCategories.findByIdAndUpdate(
@@ -45,31 +34,73 @@ const updatePostCategory = asyncHandeler(async (req, res, next) => {
       new: true,
     }
   );
-
+  
   if (!updatedPostCategory)
     throw new apiError(500, "post could not be updated");
-
+  
   res
-    .status(200)
-    .json(new apiResponse(200, updatedPostCategory, "post updated"));
+  .status(200)
+  .json(new apiResponse(200, updatedPostCategory, "post updated"));
 });
 
 const deletePostCategory = asyncHandeler(async (req, res, next) => {
   const categoryId = req.params.postCategoryId;
-
+  
   await Post.updateMany(
     { categories: { $in: [categoryId] } },
     { $pull: { categories: categoryId } }
   );
-
+  
   const response = await PostCategories.deleteOne({ _id: categoryId });
   if (!response) throw new apiError(500, "could not delete Post category");
   res.status(200).json(new apiResponse(200, response, "deleted Post category"));
 });
+
+const getAllPostCategory = asyncHandeler(async (req, res, next) => {
+  const filter = req?.query?.search;
+  let where = {};
+  if (filter) {
+    where.title = { $regex: filter, $options: "i" };
+  }
+
+  let query = PostCategories.find(where);
+  const page = parseInt(req.query?.page) || 1;
+  const pageSize = parseInt(req.query?.pageSize) || 10;
+  const skip = (page - 1) * pageSize;
+  const total = await Post.find(where).countDocuments();
+  const pages = Math.ceil(total / pageSize);
+
+  const postCategories = await query
+    .skip(skip)
+    .limit(pageSize)
+    .sort({ updatedAt: "desc" });
+  if (!postCategories) {
+    throw new apiError(404, "no post Category found");
+  }
+
+  res
+    .status(200)
+    .header({
+      "x-filetr": filter,
+      "x-totalCount": JSON.stringify(total),
+      "x-currentPage": JSON.stringify(page),
+      "x-pageSize": JSON.stringify(pageSize),
+      "x-totalPagesCount": JSON.stringify(pages),
+    })
+    .json(new apiResponse(200, postCategories, "all Post Categories"));
+});
+
+const getPostCategory = asyncHandeler ( async (req, res, next) => {
+  const postCategory = await PostCategories.findOne({_id: req?.params?.postCategoryId})
+  if(!postCategory) throw new apiError(404, "no post category found");
+  res.status(200).json(new apiResponse(200, postCategory, "category found"))
+})
+
 
 export {
   createPostCategory,
   getAllPostCategory,
   updatePostCategory,
   deletePostCategory,
+  getPostCategory,
 };
--- src/routes/postCategories.routes.js
+++ src/routes/postCategories.routes.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { createPostCategory, deletePostCategory, getAllPostCategory, updatePostCategory } from "../controllers/postCategory.controller.js";
+import { createPostCategory, deletePostCategory, getAllPostCategory, getPostCategory, updatePostCategory } from "../controllers/postCategory.controller.js";
 import { varifyJwt, varifyAdmin } from "../middleware/auth.middleware.js";
 
 const PostCatagoriesRouter = Router()
@@ -8,5 +8,6 @@ PostCatagoriesRouter.route("/").post(varifyJwt, varifyAdmin, createPostCategory)
 PostCatagoriesRouter.route("/").get( getAllPostCategory)
 PostCatagoriesRouter.route("/:postCategoryId").put(varifyJwt, varifyAdmin, updatePostCategory)
 PostCatagoriesRouter.route("/:postCategoryId").delete(varifyJwt, varifyAdmin, deletePostCategory)
+PostCatagoriesRouter.route("/:postCategoryId").get(varifyJwt, varifyAdmin, getPostCategory)
 
 export default PostCatagoriesRouter
