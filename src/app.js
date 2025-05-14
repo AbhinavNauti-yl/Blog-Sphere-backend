d3a42c0cd297ccbd85b14325e12580b759b1f730
AbhinavNauti-yl <abhinavnautiyal2004@gmail.com>
2025-05-15 02:33:37 +0530

    fix: enable credentials for CORS configuration

--- src/app.js
+++ src/app.js
@@ -20,7 +20,7 @@ app.use(
 app.use(
   cors({
     origin: "https://blog-sphere-t65e.onrender.com",
-    // credentials: true,
+    credentials: true,
     // methods: ["GET", "POST", "PUT", "DELETE"],
     // allowedHeaders: ["Content-Type", "Authorization"],
     exposedHeaders: '*',
