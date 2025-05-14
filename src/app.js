a2a038218bb5e08d42ba65f9d29ac20e4ce60a52
AbhinavNauti-yl <abhinavnautiyal2004@gmail.com>
2025-05-15 02:33:18 +0530

    refactor: clean up code formatting and restore CORS configuration

--- src/app.js
+++ src/app.js
@@ -1,41 +1,42 @@
-import express from 'express'
-import cookieparser from 'cookie-parser'
-import cors from 'cors'
+import express from "express";
+import cookieparser from "cookie-parser";
+import cors from "cors";
 
+import userRouter from "./routes/user.routes.js";
+import postRouter from "./routes/post.routes.js";
+import commentRouter from "./routes/comment.routes.js";
+import PostCatagoriesRouter from "./routes/postCategories.routes.js";
 
-import userRouter from './routes/user.routes.js'
-import postRouter from './routes/post.routes.js'
-import commentRouter from './routes/comment.routes.js'
-import PostCatagoriesRouter from './routes/postCategories.routes.js'
+const app = express();
 
-const app = express()
+app.use(express.json());
 
-app.use(express.json())
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
 
-app.use(express.urlencoded(
-    {
-        extended: true
-    }
-))
+app.use(
+  cors({
+    origin: "https://blog-sphere-t65e.onrender.com",
+    // credentials: true,
+    // methods: ["GET", "POST", "PUT", "DELETE"],
+    // allowedHeaders: ["Content-Type", "Authorization"],
+    exposedHeaders: '*',
+  })
+);
 
+app.use(cookieparser());
+app.use(express.static("public"));
 
-
-app.use(cors({
-    origin: 'https://blog-sphere-t65e.onrender.com',
-    credentials: true,
-    exposedHeaders: "https://blog-sphere-t65e.onrender.com"
-}))
-
-app.use(cookieparser())
-app.use(express.static("public"))
-
-app.use("/api/users", userRouter)
-app.use("/api/posts", postRouter)
-app.use("/api/comments", commentRouter)
-app.use("/api/postCategory", PostCatagoriesRouter)
+app.use("/api/users", userRouter);
+app.use("/api/posts", postRouter);
+app.use("/api/comments", commentRouter);
+app.use("/api/postCategory", PostCatagoriesRouter);
 
 app.get("/", (req, res) => {
-    res.send("api working")
-})
+  res.send("api working");
+});
 
-export default app
+export default app;
